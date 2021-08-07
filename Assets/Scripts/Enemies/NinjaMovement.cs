2faf6fc0efa5b546da3195845d8a0a0640755946
TheCopyNinjaDev <sparkcolazet@gmail.com>
2021-08-07 23:36:11 +0300

    Turning
    * npc knows where to go now
    
    * ninjas now are turned to a right direction
    
    * other changes
    
    * turning thing

--- Assets/Scripts/Enemies/NinjaMovement.cs
+++ Assets/Scripts/Enemies/NinjaMovement.cs
@@ -1,13 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace Enemies
-{
+{ 
+    
     public class NinjaMovement : MonoBehaviour 
     {
 
         [FormerlySerializedAs("movement_speed")] public float movementSpeed = 7f;
-        [FormerlySerializedAs("move_dir")] public float moveDir = -1f;
+        private int _moveDir;
 
         private Animator _animator;
         private static readonly int Speed = Animator.StringToHash("speed");
@@ -21,19 +23,52 @@ namespace Enemies
 
         private void FixedUpdate()
         {
+            // Applies direction
+            _moveDir = ChooseDirection();
+            
             // Moving NPCs
-            Move();
+            Move(_moveDir);
+            
+            // Rotates NPCs
+            TurnToDirection(_moveDir);
         }
 
         /// <summary>
         /// Moves NPCs to move direction
         /// </summary>
-        private void Move()
+        /// <param name="direction">direction he needs to go</param>
+        private void Move(int direction)
         {
             var position = transform.position;
             // ReSharper disable once Unity.InefficientPropertyAccess
-            transform.position = new Vector3(position.x + movementSpeed * moveDir, position.y);
+            transform.position = new Vector3(position.x + movementSpeed * direction, position.y);
             _animator.SetFloat(Speed, 1f);
+        }
+
+        /// <summary>
+        /// Turns npc to a headed direction
+        /// </summary>
+        /// <param name="direction">The direction in which npc needs to be turned</param>
+        private void TurnToDirection(int direction)
+        {
+            switch (direction)
+            {
+                case 1:
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                    break;
+                case -1:
+                    transform.rotation = Quaternion.Euler(0, 180, 0);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Defines in which direction npc needs to go
+        /// </summary>
+        /// <returns>The direction npc needs to go</returns>
+        private int ChooseDirection()
+        {
+            return -Math.Sign(transform.position.x);
         }
     }
 }
